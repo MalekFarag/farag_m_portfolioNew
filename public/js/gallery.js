d8fb7680bca633e82c71f22bd4e2f946d3ffc022
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-12-02 15:55:57 -0500

    some changes

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,1 +1,29 @@
 //Gallery Functions
+//lightbox
+
+
+
+
+
+// portfolio switch functions
+    const projList = document.querySelectorAll('.projList li'),
+          projDivs = document.querySelectorAll('.projView .projInfo');
+
+          function listClick(e){
+              let itemName = this.classList[1];
+              console.log(projDivs);
+              projList.forEach(item => item.classList.remove('active'));
+              document.querySelector('.' + itemName).classList.add('active');
+          }
+
+          function projSwap(e){
+            let divName = this.classList[1].split('li')[1];
+            let projName = '.proj' + divName;
+
+            projDivs.forEach(div => div.classList.remove('view'));
+
+            document.querySelector(projName).classList.add('view');
+          }
+
+          projList.forEach( item => item.addEventListener('click', listClick));
+          projList.forEach( item => item.addEventListener('click', projSwap));
