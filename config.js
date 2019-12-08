1ca3155b3a15df1f0a102c83c40bc6888b2e7a9a
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-12-07 22:36:22 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
-const config = {
-    host: 'localhost',
-    port: 8889, // windows: 3306 // MAC: 8889 or 8888 // *Change accordingly
-    uname: 'root',
-    pname: 'root', // windows: *leave blank* // MAC: root // *Change accordingly
-    db: 'db_malekf_site'
-}
+//const config = {
+  //  host: 'localhost',
+    //port: 8889, // windows: 3306 // MAC: 8889 or 8888 // *Change accordingly
+    //uname: 'root',
+    //pname: 'root', // windows: *leave blank* // MAC: root // *Change accordingly
+    //db: 'db_malekf_site'
+///}
 
-module.exports = config;
+//module.exports = config;
 
-// const config = {
-//     host: 'us-cdbr-iron-east-05.cleardb.net',
-//     port: 3306, // windows: 3306 // MAC: 8889 or 8888 // *Change accordingly
-//     uname: 'b272b52615187d',
-//     pname: '73901c0a', // windows: *leave blank* // MAC: root // *Change accordingly
-//     db: 'dbjdf69qftyaya'
-// }
+ const config = {
+     host: 'us-cdbr-iron-east-05.cleardb.net',
+     port: 3306, // windows: 3306 // MAC: 8889 or 8888 // *Change accordingly
+     uname: 'b272b52615187d',
+     pname: '73901c0a', // windows: *leave blank* // MAC: root // *Change accordingly
+     db: 'dbjdf69qftyaya'
+ }
 
-// module.exports = config;
+ module.exports = config;
