1d8c5ea5a6a81a522a9878c5a17dff258693af17
Malek F <43182938+MalekFarag@users.noreply.github.com>
2019-12-07 21:58:42 -0500

    site complete (needs hosting)

--- config.js
+++ config.js
@@ -7,3 +7,13 @@ const config = {
 }
 
 module.exports = config;
+
+// const config = {
+//     host: 'us-cdbr-iron-east-05.cleardb.net',
+//     port: 3306, // windows: 3306 // MAC: 8889 or 8888 // *Change accordingly
+//     uname: 'b272b52615187d',
+//     pname: '73901c0a', // windows: *leave blank* // MAC: root // *Change accordingly
+//     db: 'dbjdf69qftyaya'
+// }
+
+// module.exports = config;
