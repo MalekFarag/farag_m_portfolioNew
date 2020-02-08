908dbd59a2d26c9592e986f1345b5d07a1313fc4
Malek F <43182938+MalekFarag@users.noreply.github.com>
2020-02-08 18:52:58 -0500

    successfully implemented nodemailer

---
+++ config/mailcreds.js
@@ -0,0 +1,7 @@
+const mailcreds = {
+    service: 'gmail',
+    user: 'faragmalek14@gmail.com', // your email (gmail) goes here, don't use mine
+    pass: 'Malooky14' // see above
+}
+
+module.exports = mailcreds;
